90be4cc7ea3d1857fd99cd485daa31afdad8ea8a
Strunk <strunk@MacBook-Pro.local>
2022-05-03 12:38:44 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Menu from "./menu/Menu";
+
+// Array of objects
+const menuItems = [
+  {
+    id: 1,
+    type: 'Slice of Vegan Cheese',
+    price: 4,
+    qty: 0
+  },
+  {
+    id: 2,
+    type: 'Slice of Mushroom',
+    price: 3,
+    qty: 0
+  },
+  {
+    id: 3,
+    type: 'Slice of Chicken and Bacon',
+    price: 3,
+    qty: 0
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>Hello from the App component</h1> */}
+      {/* Passing the menuItems array (Array of objects) as the value for the startingItems prop in the Menu Component */}
+      <Menu startingItems={menuItems}/>
     </div>
   );
 }
